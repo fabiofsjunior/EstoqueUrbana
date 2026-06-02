e10423910604457e4517d0d4abaa2b49ea3fc232
FábioJrDev <fabiojrexecutivo@gmail.com>
2026-06-02 18:05:26 -0300

    test fix indicador

--- js/api.js
+++ js/api.js
@@ -1,19 +1,33 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbwgwU6zyMYbrWtCO6ORZJ2-5CfHJ0-kEa4QmAYbCtDyEOvrbOow4ergdID3vxzD_zEv/exec";
+function api(action) {
+  return new Promise((resolve, reject) => {
+    const callbackName = "jsonp_" + Date.now();
 
-async function api(action) {
-  try {
-    const res = await fetch(`${API_URL}?action=${action}`);
-    
-    if (!res.ok) {
-      throw new Error("Erro HTTP: " + res.status);
-    }
+    window[callbackName] = function (data) {
+      resolve(data);
 
-    const data = await res.json();
-    return data;
+      delete window[callbackName];
 
-  } catch (err) {
-    console.error("Erro API:", err);
-    throw err;
-  }
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+
+    const script = document.createElement("script");
+
+    script.src = `${API_URL}?action=${action}&callback=${callbackName}`;
+
+    script.onerror = function () {
+      reject(new Error("Falha ao carregar API"));
+
+      delete window[callbackName];
+
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+
+    document.body.appendChild(script);
+  });
 }
