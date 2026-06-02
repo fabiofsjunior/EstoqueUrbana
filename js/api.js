11fe2624392f3dbfef84e5745b42aa6f09237918
FábioJrDev <fabiojrexecutivo@gmail.com>
2026-06-02 18:10:28 -0300

    fix fecth

--- js/api.js
+++ js/api.js
@@ -1,10 +1,15 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbwgwU6zyMYbrWtCO6ORZJ2-5CfHJ0-kEa4QmAYbCtDyEOvrbOow4ergdID3vxzD_zEv/exec";
-function api(action) {
-  return new Promise((resolve, reject) => {
-    const callbackName = "jsonp_" + Date.now();
 
-    window[callbackName] = function (data) {
+function api(action) {
+
+  return new Promise((resolve, reject) => {
+
+    const callbackName =
+      "cb_" + Date.now();
+
+    window[callbackName] = function(data) {
+
       resolve(data);
 
       delete window[callbackName];
@@ -12,22 +17,29 @@ function api(action) {
       if (script.parentNode) {
         script.parentNode.removeChild(script);
       }
+
     };
 
-    const script = document.createElement("script");
+    const script =
+      document.createElement("script");
 
-    script.src = `${API_URL}?action=${action}&callback=${callbackName}`;
+    script.src =
+      `${API_URL}?action=${action}&callback=${callbackName}`;
 
-    script.onerror = function () {
-      reject(new Error("Falha ao carregar API"));
+    script.onerror = function(err) {
+
+      reject(err);
 
       delete window[callbackName];
 
       if (script.parentNode) {
         script.parentNode.removeChild(script);
       }
+
     };
 
     document.body.appendChild(script);
+
   });
+
 }
