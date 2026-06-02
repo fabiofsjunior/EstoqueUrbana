e58582282cdb1aed51778a1b42fb980f26cc4024
FábioJrDev <fabiojrexecutivo@gmail.com>
2026-06-02 09:47:44 -0300

    url change

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxS_UdM5NZQmnNLrUrLfBUzDKn3z0r2nNqqmac8MYfjNnuV8mNtVHT7g8BdTys3vC9q/exec";
+"https://script.google.com/macros/s/AKfycbyo94qufqodAlclD09ugxX1hphSzLcfMQeRVGfRF1XtBNk6roL6z_aIdqG7yscxPcXk/exec";
